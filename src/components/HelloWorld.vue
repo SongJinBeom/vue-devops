59dfa6fb860c48e9f19e9fbbb812819799fc6d24
Song <43747415+SongJinBeom@users.noreply.github.com>
2021-06-29 15:31:15 +0900

    TEST

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <h1>테스트 실패 연습</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br />
       check out the
@@ -9,6 +10,8 @@
       >.
     </p>
     <h3>Installed CLI Plugins</h3>
+
+    
     <ul>
       <li>
         <a
